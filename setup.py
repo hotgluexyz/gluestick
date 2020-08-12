83f4bcfac1c621db0d6a1897c75be78dc8b006c7
Hassan Syyid <hassansyyid@gmail.com>
2020-08-12 14:41:07 -0400

    Update setup.py and LICENSE

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='gluestick',
     version='1.0.0',
     description='ETL utility functions built on Pandas',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/hotgluexyz/gluestick',
     author='hotglue',
     author_email='hello@hotglue.xyz',
