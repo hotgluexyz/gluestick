b3901234bd06b3afacb9892845773c6205fc7157
Hassan Syyid <hassansyyid@gmail.com>
2020-08-25 12:33:02 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='gluestick',
-    version='1.0.0',
+    version='1.0.1',
     description='ETL utility functions built on Pandas',
     long_description=long_description,
     long_description_content_type="text/markdown",
