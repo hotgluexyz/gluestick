a6bee5e7e869b272063130c3008571d727cccaa1
Umar Syyid <usyyid@buckitapp.co>
2020-09-03 17:22:56 -0400

    Add required packages in setup

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/hotgluexyz/gluestick',
+    install_requires=[
+        'xlrd>=1.0.0',
+        'numpy>=1.14.3',
+        'pandas>=0.23.0',
+    ],
     author='hotglue',
     author_email='hello@hotglue.xyz',
     license='MIT',
