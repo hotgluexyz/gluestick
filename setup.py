1f59cc26bcba86d5d0e296385f3243838f717470
Keyna Rafael <95432445+keyn4@users.noreply.github.com>
2024-02-05 12:13:33 -0500

    add to_export (#24)
    ---------
    
    Co-authored-by: Hassan Syyid <hassansyyid@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="gluestick",
-    version="2.1.7",
+    version="2.1.8",
     description="ETL utility functions built on Pandas",
     long_description=long_description,
     long_description_content_type="text/markdown",
