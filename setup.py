b28be20c6084ec5a44357b7438abcd5d6897795b
Key <keynarafaelp@gmail.com>
2024-12-02 14:37:28 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="gluestick",
-    version="2.1.34",
+    version="2.2.0",
     description="ETL utility functions built on Pandas",
     long_description=long_description,
     long_description_content_type="text/markdown",
