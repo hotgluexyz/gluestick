e0e2b5d0e56192cf79e0dbe42b7a7cc6270e523d
Lior Ben-David <liorbd@Outlook.com>
2025-11-14 14:33:23 -0500

    Add snapshot functions to Polars Readers (#77)

--- gluestick/readers/pl_reader.py
+++ gluestick/readers/pl_reader.py
@@ -3,6 +3,7 @@ from gluestick.utils.polars_utils import map_pd_type_to_polars, cast_df_from_sch
 import pyarrow.parquet as pq
 import polars as pl
 import pandas as pd
+import os
 
 
 class PolarsReader(Reader):
@@ -65,3 +66,96 @@ class PolarsReader(Reader):
             for k, v in pd_types.items()
         }
         return {col: map_pd_type_to_polars(pd_type) for col, pd_type in pd_types.items()}
+
+    def read_snapshots(self,stream, snapshot_dir, **kwargs) -> pl.DataFrame | None:
+        """Read a snapshot file and return a polars dataframe.
+
+        Parameters
+        ----------
+        stream: str
+            The name of the stream to read the snapshot from.
+        snapshot_dir: str
+            The path to the snapshot directory.
+        """
+        if os.path.isfile(path=f"{snapshot_dir}/{stream}.snapshot.parquet"):
+            return pl.read_parquet(source=f"{snapshot_dir}/{stream}.snapshot.parquet", **kwargs)
+        elif os.path.isfile(path=f"{snapshot_dir}/{stream}.snapshot.csv"):
+            return pl.read_csv(source=f"{snapshot_dir}/{stream}.snapshot.csv", **kwargs)
+        else:
+            return None
+
+    def snapshot_records(
+        self,
+        stream_data,
+        stream,
+        snapshot_dir,
+        pk="id", 
+        just_new=False, 
+        use_csv=False, 
+        overwrite=False, 
+    ) -> pl.DataFrame | None:
+        """Update a snapshot file and return the merged data.
+
+        Parameters
+        ----------
+        stream_data: pl.DataFrame
+            The data to be included in the snapshot.
+        stream: str
+            The name of the stream of the snapshots.
+        snapshot_dir: str
+            The name of the stream of the snapshots.
+        pk: str or list of str
+            The primary key used for the snapshot.
+        just_new: bool
+            Return just the input data if True, else returns the whole data
+        use_csv: bool
+            Whether to use csv format for the snapshot instead of parquet.
+        overwrite: bool
+            Whether to overwrite the existing snapshot file instead of updating and merging.
+
+        Returns
+        -------
+        return: pl.DataFrame
+            A polars dataframe with the merged data.
+
+        """
+
+        if isinstance(pk, str):
+            pk = [pk]
+
+        snapshot_df = self.read_snapshots(stream, snapshot_dir)
+        if not overwrite and stream_data is not None and snapshot_df is not None:
+            
+            for key in pk:
+                new_data_pk_df = stream_data.select(key)
+                snapshot_df = snapshot_df.filter(
+                    ~pl.col(key).is_in(new_data_pk_df.get_column(key))
+                )
+
+
+            merged_df = pl.concat(items=[snapshot_df, stream_data], how="diagonal_relaxed")
+
+            if use_csv:
+                merged_df.write_csv(f"{snapshot_dir}/{stream}.snapshot.csv")
+            else:
+                merged_df.write_parquet(f"{snapshot_dir}/{stream}.snapshot.parquet")
+            
+
+            if just_new:
+                return stream_data
+            else:
+                return merged_df
+        elif stream_data is not None:
+            if use_csv:
+                stream_data.write_csv(f"{snapshot_dir}/{stream}.snapshot.csv")
+            else:
+                stream_data.write_parquet(f"{snapshot_dir}/{stream}.snapshot.parquet")
+
+            return stream_data
+        elif snapshot_df is not None:
+            return snapshot_df
+        else:
+            return None
+
+
+
