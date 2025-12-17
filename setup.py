e94b5a63d40d8c11a648962fae95c8be1aa4899a
LiorB-D <liorbd@outlook.com>
2025-12-17 17:12:38 -0500

    BUmp verison

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="gluestick",
-    version="3.0.2",
+    version="3.0.3",
     description="ETL utility functions built for the hotglue iPaaS platform",
     long_description=long_description,
     long_description_content_type="text/markdown",
