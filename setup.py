e39be9d496e880af72166167a152c88955eef1c5
Mike Weaver <mikeweaverg@gmail.com>
2026-03-12 09:19:25 -0400

    feat/hgi-9861 fix: fix over-filtering associated with polars in gluestick  (#86)
    * fix: fix over-filtering associated with polars in gluestick
    
    * fix: update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="gluestick",
-    version="3.0.5",
+    version="3.0.6",
     description="ETL utility functions built for the hotglue iPaaS platform",
     long_description=long_description,
     long_description_content_type="text/markdown",
